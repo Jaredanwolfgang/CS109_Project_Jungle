87d0bf591f0ca507c721e741bf94a0c38781af49
2272629813@qq.com <2272629813@qq.com>
2023-04-24 21:32:35 +0800

    The original project file with declared packages.

---
+++ src/listener/GameListener.java
@@ -0,0 +1,13 @@
+package listener;
+
+import model.ChessBoard.ChessboardPoint;
+import view.CellComponent;
+import view.ChessComponent.ElephantChessComponent;
+
+public interface GameListener {
+
+    void onPlayerClickCell(ChessboardPoint point, CellComponent component);
+
+    void onPlayerClickChessPiece(ChessboardPoint point, ElephantChessComponent component);
+
+}
---
+++ src/model/ChessPieces/ChessPiece.java
@@ -0,0 +1,32 @@
+package model.ChessPieces;
+
+
+import model.Enum.PlayerColor;
+
+public class ChessPiece {
+    // the owner of the chess
+    private PlayerColor owner;
+
+    // Elephant? Cat? Dog? ...
+    private String name;
+    private int rank;
+
+    public ChessPiece(PlayerColor owner, String name, int rank) {
+        this.owner = owner;
+        this.name = name;
+        this.rank = rank;
+    }
+
+    public boolean canCapture(ChessPiece target) {
+        // TODO: Finish this method!
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PlayerColor getOwner() {
+        return owner;
+    }
+}
