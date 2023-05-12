c96954112c978f52bc4e26daa5d2cbbce04c8041
2272629813@qq.com <2272629813@qq.com>
2023-05-12 19:48:31 +0800

    I have finished the RankList, you can modify based on this version.

--- src/model/AI/AI_Hard.java
+++ src/model/AI/AI_Hard.java
@@ -8,18 +8,16 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 
 public class AI_Hard {
-    static final int NUMBER_OF_ITERATIONS = 10000;
-    static HashMap<CellGrid, ArrayList<Move>> historyMoves = new HashMap<>();
+    static final int NUMBER_OF_ITERATIONS = 20000;
 
     public static Move findBestOneMove(Cell[][] board, Color player) {
         long current1 = System.currentTimeMillis();
         Cell[][] boardCopy = Chessboard.cloneBoard(board);
         MonteCarloTreeSearch mcts = new MonteCarloTreeSearch();
         mcts.root = new Node(player, null, boardCopy, null);
-        mcts.findBestMove(NUMBER_OF_ITERATIONS);
+        mcts.findBestMove();
         long current2 = System.currentTimeMillis();
         System.out.printf("Time needed for %d iterations is %f seconds\n", NUMBER_OF_ITERATIONS, (current2 - current1) / 1000.0d);
         return mcts.bestMove.move;
@@ -58,7 +56,7 @@ class MonteCarloTreeSearch {
     }
 
     void generateChildren(Node node) {
-        ArrayList<Move> moves = JungleSimulator.getAvailableMoves(node.board, node.player, 0);
+        ArrayList<Move> moves = JungleSimulator.getAvailableMoves(node.board, node.player);
         for (Move move : moves) {
             Cell[][] newBoard = Chessboard.cloneBoard(node.board);
             if (move.isDoesCapture()) {
@@ -91,8 +89,8 @@ class MonteCarloTreeSearch {
         }
     }
 
-    void findBestMove(int numIteration) {
-        for (int i = 0; i < numIteration; i++) {
+    void findBestMove() {
+        for (int i = 0; i < AI_Hard.NUMBER_OF_ITERATIONS; i++) {
             Node rolloutNode = findRolloutNode();
             Color winner = JungleSimulator.rollout(rolloutNode);
             backPropagate(rolloutNode, winner);
@@ -109,8 +107,6 @@ class MonteCarloTreeSearch {
             System.out.println(Arrays.toString(new String[]{child.visits + "  " + child.wins + "  " + child.losses + "  " + child.UCTValue + "  " + child.move}));
         }
         System.out.println();
-        //simulator.printGame(bestMove.board);
-        //System.out.println();
     }
 }
 
@@ -135,7 +131,7 @@ class Node implements Comparable<Node> {
         if (visits == 0) {
             UCTValue = Double.MAX_VALUE;
         } else {
-            UCTValue = (wins - losses) / visits + Math.sqrt(2 * Math.log(parent.visits) / visits);
+            UCTValue = wins / visits + Math.sqrt(2 * Math.log(parent.visits) / visits);
         }
     }
 
@@ -154,79 +150,14 @@ class JungleSimulator {
         }
         Color player = node.player;
         Cell[][] currentBoard = Chessboard.cloneBoard(node.board);
-        int[] bluePieces = new int[9];
-        int[] redPieces = new int[9];
-        int maxBlue = 0;
-        int maxRed = 0;
-        int minBlue = 8;
-        int minRed = 8;
-        int count = 0;
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 7; j++) {
-                if (currentBoard[i][j].getPiece() != null) {
-                    if (currentBoard[i][j].getPiece().getOwner().getColor() == Color.BLUE) {
-                        bluePieces[currentBoard[i][j].getPiece().getRank()]++;
-                        maxBlue = Math.max(maxBlue, currentBoard[i][j].getPiece().getRank());
-                        minBlue = Math.min(minBlue, currentBoard[i][j].getPiece().getRank());
-                    } else {
-                        redPieces[currentBoard[i][j].getPiece().getRank()]++;
-                        maxRed = Math.max(maxRed, currentBoard[i][j].getPiece().getRank());
-                        minRed = Math.min(minRed, currentBoard[i][j].getPiece().getRank());
-                    }
-                }
-            }
-        }
         while (true) {
-            ArrayList<Move> moves = JungleSimulator.getAvailableMoves(currentBoard, player, count);
+            ArrayList<Move> moves = JungleSimulator.getAvailableMoves(currentBoard, player);
             if (moves.isEmpty()) {
                 return JungleSimulator.flipColor(player);
             }
             int randomMoveIndex = (int) (Math.random() * moves.size());
             Move moveToMake = moves.get(randomMoveIndex);
             if (moveToMake.isDoesCapture()) {
-                if (flipColor(player) == Color.BLUE) {
-                    bluePieces[currentBoard[moveToMake.getToPoint().getRow()][moveToMake.getToPoint().getCol()].getPiece().getRank()]--;
-                    if (currentBoard[moveToMake.getToPoint().getRow()][moveToMake.getToPoint().getCol()].getPiece().getRank() == maxBlue) {
-                        for (int i = maxBlue - 1; i >= 0; i--) {
-                            if (bluePieces[i] > 0) {
-                                maxBlue = i;
-                                break;
-                            }
-                        }
-                    }
-                    if (currentBoard[moveToMake.getToPoint().getRow()][moveToMake.getToPoint().getCol()].getPiece().getRank() == minBlue) {
-                        for (int i = minBlue + 1; i < 9; i++) {
-                            if (bluePieces[i] > 0) {
-                                minBlue = i;
-                                break;
-                            }
-                        }
-                    }
-                } else {
-                    redPieces[currentBoard[moveToMake.getToPoint().getRow()][moveToMake.getToPoint().getCol()].getPiece().getRank()]--;
-                    if (currentBoard[moveToMake.getToPoint().getRow()][moveToMake.getToPoint().getCol()].getPiece().getRank() == maxRed) {
-                        for (int i = maxRed - 1; i >= 0; i--) {
-                            if (redPieces[i] > 0) {
-                                maxRed = i;
-                                break;
-                            }
-                        }
-                    }
-                    if (currentBoard[moveToMake.getToPoint().getRow()][moveToMake.getToPoint().getCol()].getPiece().getRank() == minRed) {
-                        for (int i = minRed + 1; i < 9; i++) {
-                            if (redPieces[i] > 0) {
-                                minRed = i;
-                                break;
-                            }
-                        }
-                    }
-                }
-                if (minRed > maxBlue) {
-                    return Color.RED;
-                }
-                if (minBlue > maxRed) {
-                    return Color.BLUE;
-                }
                 currentBoard[moveToMake.getToPoint().getRow()][moveToMake.getToPoint().getCol()].removePiece();
             }
             currentBoard[moveToMake.getToPoint().getRow()][moveToMake.getToPoint().getCol()].setPiece(currentBoard[moveToMake.getFromPoint().getRow()][moveToMake.getFromPoint().getCol()].getPiece());
@@ -243,55 +174,45 @@ class JungleSimulator {
             }
             if (player == Color.BLUE) {
                 if (currentBoard[8][4].getPiece() != null && currentBoard[8][4].getPiece().getOwner().getColor() == Color.BLUE &&
-                        (currentBoard[8][5].getPiece() == null || currentBoard[8][4].getPiece().canCapture(currentBoard[8][5].getPiece())) &&
-                        (currentBoard[7][4].getPiece() == null || currentBoard[8][4].getPiece().canCapture(currentBoard[7][4].getPiece()))) {
+                        (currentBoard[8][5].getPiece() == null || currentBoard[8][4].getPiece().getOwner().getColor() == Color.BLUE) &&
+                        (currentBoard[7][4].getPiece() == null || currentBoard[8][4].getPiece().getOwner().getColor() == Color.BLUE)) {
                     return player;
                 }
                 if (currentBoard[8][2].getPiece() != null && currentBoard[8][2].getPiece().getOwner().getColor() == Color.BLUE &&
-                        (currentBoard[8][1].getPiece() == null || currentBoard[8][2].getPiece().canCapture(currentBoard[8][1].getPiece())) &&
-                        (currentBoard[7][2].getPiece() == null || currentBoard[8][2].getPiece().canCapture(currentBoard[7][2].getPiece()))) {
+                        (currentBoard[8][1].getPiece() == null || currentBoard[8][2].getPiece().getOwner().getColor() == Color.BLUE) &&
+                        (currentBoard[7][2].getPiece() == null || currentBoard[8][2].getPiece().getOwner().getColor() == Color.BLUE)) {
                     return player;
                 }
                 if (currentBoard[7][3].getPiece() != null && currentBoard[7][3].getPiece().getOwner().getColor() == Color.BLUE &&
-                        (currentBoard[6][3].getPiece() == null || currentBoard[7][3].getPiece().canCapture(currentBoard[6][3].getPiece())) &&
-                        (currentBoard[7][2].getPiece() == null || currentBoard[7][3].getPiece().canCapture(currentBoard[7][2].getPiece())) &&
-                        (currentBoard[7][4].getPiece() == null || currentBoard[7][3].getPiece().canCapture(currentBoard[7][4].getPiece()))) {
+                        (currentBoard[6][3].getPiece() == null || currentBoard[7][3].getPiece().getOwner().getColor() == Color.BLUE) &&
+                        (currentBoard[7][2].getPiece() == null || currentBoard[7][3].getOwner().getColor() == Color.BLUE) &&
+                        (currentBoard[7][4].getPiece() == null || currentBoard[7][3].getOwner().getColor() == Color.BLUE)) {
                     return player;
                 }
             }
             if (player == Color.RED) {
                 if (currentBoard[0][4].getPiece() != null && currentBoard[0][4].getPiece().getOwner().getColor() == Color.RED &&
-                        (currentBoard[0][5].getPiece() == null || currentBoard[0][4].getPiece().canCapture(currentBoard[0][5].getPiece())) &&
-                        (currentBoard[1][4].getPiece() == null || currentBoard[0][4].getPiece().canCapture(currentBoard[1][4].getPiece()))) {
+                        (currentBoard[0][5].getPiece() == null || currentBoard[0][4].getPiece().getOwner().getColor() == Color.RED) &&
+                        (currentBoard[1][4].getPiece() == null || currentBoard[0][4].getPiece().getOwner().getColor() == Color.RED)) {
                     return player;
                 }
                 if (currentBoard[0][2].getPiece() != null && currentBoard[0][2].getPiece().getOwner().getColor() == Color.RED &&
-                        (currentBoard[0][1].getPiece() == null || currentBoard[0][2].getPiece().canCapture(currentBoard[0][1].getPiece())) &&
-                        (currentBoard[1][2].getPiece() == null || currentBoard[0][2].getPiece().canCapture(currentBoard[1][2].getPiece()))) {
+                        (currentBoard[0][1].getPiece() == null || currentBoard[0][2].getPiece().getOwner().getColor() == Color.RED) &&
+                        (currentBoard[1][2].getPiece() == null || currentBoard[0][2].getPiece().getOwner().getColor() == Color.RED)) {
                     return player;
                 }
                 if (currentBoard[1][3].getPiece() != null && currentBoard[1][3].getPiece().getOwner().getColor() == Color.RED &&
-                        (currentBoard[2][3].getPiece() == null || currentBoard[1][3].getPiece().canCapture(currentBoard[2][3].getPiece())) &&
-                        (currentBoard[1][2].getPiece() == null || currentBoard[1][3].getPiece().canCapture(currentBoard[1][2].getPiece())) &&
-                        (currentBoard[1][4].getPiece() == null || currentBoard[1][3].getPiece().canCapture(currentBoard[1][4].getPiece()))) {
+                        (currentBoard[2][3].getPiece() == null || currentBoard[1][3].getPiece().getOwner().getColor() == Color.RED) &&
+                        (currentBoard[1][2].getPiece() == null || currentBoard[1][3].getPiece().getOwner().getColor() == Color.RED) &&
+                        (currentBoard[1][4].getPiece() == null || currentBoard[1][3].getPiece().getOwner().getColor() == Color.RED)) {
                     return player;
                 }
             }
             player = flipColor(player);
-            count++;
         }
     }
 
-    static ArrayList<Move> getAvailableMoves(Cell[][] board, Color player, int depth) {
-        /*if(depth < 10){
-            CellGrid cells = new CellGrid(Chessboard.cloneBoard(board));
-            if (AI_MCTS.historyMoves.containsKey(cells)) {
-                return AI_MCTS.historyMoves.get(cells);
-            }
-            ArrayList<Move> moves = Chessboard.getAllPossibleMoveOnBoard(board, player);
-            AI_MCTS.historyMoves.put(cells, moves);
-            return moves;
-        }*/
+    static ArrayList<Move> getAvailableMoves(Cell[][] board, Color player) {
         return Chessboard.getAllPossibleMoveOnBoard(board, player);
     }
 
@@ -302,7 +223,7 @@ class JungleSimulator {
         if (player == Color.RED && board[0][3].getPiece() != null && board[0][3].getPiece().getOwner().getColor() == Color.RED) {
             return player;
         }
-        /*boolean opponentHasNoPiece = true;
+        boolean opponentHasNoPiece = true;
         Color opponent = flipColor(player);
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 7; j++){
@@ -314,7 +235,7 @@ class JungleSimulator {
         }
         if(opponentHasNoPiece){
             return player;
-        }*/
+        }
         return GAME_CONTINUES;
     }
 
@@ -323,43 +244,5 @@ class JungleSimulator {
             return Color.RED;
         }
         return Color.BLUE;
-    }
-}
-
-class CellGrid {
-    private Cell[][] cells;
-
-    public CellGrid(Cell[][] cells) {
-        this.cells = cells;
-    }
-
-    public Cell[][] getCells() {
-        return cells;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof CellGrid)) {
-            return false;
-        }
-        CellGrid other = (CellGrid) o;
-        boolean isEqual = true;
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 7; j++) {
-                if (!cells[i][j].equals(other.getCells()[i][j])) {
-                    isEqual = false;
-                    break;
-                }
-            }
-        }
-        return isEqual;
-    }
-
-    @Override
-    public int hashCode() {
-        return Arrays.deepHashCode(cells);
     }
 }
