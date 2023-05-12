bd26abcec57dc58c0c9ee251931f16d61a8af782
Cypher Bruce <12212006@mail.sustech.edu.cn>
2023-05-12 15:49:48 +0800

    *

--- src/view/Frame/InitFrame.java
+++ src/view/Frame/InitFrame.java
@@ -3,12 +3,15 @@ package view.Frame;
 import listener.GameListener;
 
 import javax.swing.*;
-import javax.swing.text.View;
 import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.Timer;
 
 public class InitFrame extends JFrame{
+    public static double initFrameResize = 1.0;
     private final Dimension screenSize = new Dimension(800,600);
     private final JButton loginButton = new JButton();
     private final JButton registerButton = new JButton();
@@ -28,6 +31,41 @@ public class InitFrame extends JFrame{
         initBackground("Background\\Jungle.gif");
 
         this.setVisible(false);
+
+        Timer resizeTimer = new Timer(150, e -> {
+            Dimension newDimension =  frame.getInitFrame().getSize();
+            if(newDimension.height != (int) (initFrameResize * screenSize.getHeight())){
+                initFrameResize = (double) newDimension.height / screenSize.height;
+            }else if(newDimension.width != (int) (initFrameResize * screenSize.getWidth())){
+                initFrameResize = (double) newDimension.width / screenSize.width;
+            }else{
+                return;
+            }
+            if(initFrameResize > 1.5){
+                initFrameResize = 1.5;
+            }
+            if(initFrameResize < 0.5){
+                initFrameResize = 0.5;
+            }
+            frame.getInitFrame().getContentPane().removeAll();
+            initLabel();
+            initLoginButton();
+            initRegisterButton();
+            initMusicButton();
+            initExitButton();
+            initBackground("Background\\Jungle.gif");
+            frame.getInitFrame().setSize((int) (initFrameResize * screenSize.getWidth()), (int) (initFrameResize * screenSize.getHeight()));
+        });
+
+        addComponentListener(new ComponentListener() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                resizeTimer.restart();
+            }
+            @Override public void componentMoved(ComponentEvent e) {}
+            @Override public void componentShown(ComponentEvent e) {}
+            @Override public void componentHidden(ComponentEvent e) {}
+        });
     }
 
     //初始化Frame
@@ -41,14 +79,14 @@ public class InitFrame extends JFrame{
         //this.setAlwaysOnTop(true);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setResizable(false);
+        this.setResizable(true);
     }
 
     //初始化背景，使用的是JLabel
     public void initBackground(String Address) {
 //        System.out.println("InitFrame background is initializing...");
         JLabel background = new JLabel(new ImageIcon(Address));
-        background.setBounds(0, 0, (int)screenSize.getWidth(), (int)screenSize.getHeight());
+        background.setBounds(0, 0, (int)(initFrameResize * screenSize.getWidth()), (int)(initFrameResize * screenSize.getHeight()));
         this.getContentPane().add(background);
     }
 
@@ -60,15 +98,15 @@ public class InitFrame extends JFrame{
 //        System.out.println("Login button is initializing...");
 
         /** To get the scaled Image */
-        ImageIcon Button_Light_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\InitFrame\\Login_Light.png").getScaledInstance(160, 90, Image.SCALE_SMOOTH));
-        ImageIcon Button_Dark_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\InitFrame\\Login_Dark.png").getScaledInstance(160, 90, Image.SCALE_SMOOTH));
+        ImageIcon Button_Light_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\InitFrame\\Login_Light.png").getScaledInstance((int) (initFrameResize * 160), (int) (initFrameResize * 90), Image.SCALE_SMOOTH));
+        ImageIcon Button_Dark_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\InitFrame\\Login_Dark.png").getScaledInstance((int) (initFrameResize * 160), (int) (initFrameResize * 90), Image.SCALE_SMOOTH));
 
         loginButton.setBorderPainted(false);
         loginButton.setContentAreaFilled(false);
         loginButton.setFocusPainted(false);
         loginButton.setOpaque(false);
 
-        loginButton.setBounds(100, 430, 160, 90);
+        loginButton.setBounds((int) (initFrameResize * 100), (int) (initFrameResize * 430), (int) (initFrameResize * 160), (int) (initFrameResize * 90));
         loginButton.setIcon(Button_Light_New);
         loginButton.addMouseListener(new MouseListener() {
             @Override
@@ -94,15 +132,15 @@ public class InitFrame extends JFrame{
 //        System.out.println("Register button is initializing...");
 
         /** To get the scaled Image */
-        ImageIcon Button_Light_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\InitFrame\\Register_Light.png").getScaledInstance(160, 90, Image.SCALE_SMOOTH));
-        ImageIcon Button_Dark_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\InitFrame\\Register_Dark.png").getScaledInstance(160, 90, Image.SCALE_SMOOTH));
+        ImageIcon Button_Light_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\InitFrame\\Register_Light.png").getScaledInstance((int) (initFrameResize * 160), (int) (initFrameResize * 90), Image.SCALE_SMOOTH));
+        ImageIcon Button_Dark_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\InitFrame\\Register_Dark.png").getScaledInstance((int) (initFrameResize * 160), (int) (initFrameResize * 90), Image.SCALE_SMOOTH));
 
         registerButton.setBorderPainted(false);
         registerButton.setContentAreaFilled(false);
         registerButton.setFocusPainted(false);
         registerButton.setOpaque(false);
 
-        registerButton.setBounds(540, 430, 160, 90);
+        registerButton.setBounds((int) (initFrameResize * 540), (int) (initFrameResize * 430), (int) (initFrameResize * 160), (int) (initFrameResize * 90));
         registerButton.setIcon(Button_Light_New);
         registerButton.addMouseListener(new MouseListener() {
             @Override
@@ -126,15 +164,15 @@ public class InitFrame extends JFrame{
 //        System.out.println("Music button is initializing...");
 
         /** To get the scaled Image */
-        ImageIcon Button_Light_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\AllFrame\\MusicButton_Light.png").getScaledInstance(50, 50, Image.SCALE_SMOOTH));
-        ImageIcon Button_Dark_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\AllFrame\\MusicButton_Dark.png").getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+        ImageIcon Button_Light_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\AllFrame\\MusicButton_Light.png").getScaledInstance((int) (initFrameResize * 50), (int) (initFrameResize * 50), Image.SCALE_SMOOTH));
+        ImageIcon Button_Dark_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\AllFrame\\MusicButton_Dark.png").getScaledInstance((int) (initFrameResize * 50), (int) (initFrameResize * 50), Image.SCALE_SMOOTH));
 
         musicButton.setBorderPainted(false);
         musicButton.setContentAreaFilled(false);
         musicButton.setFocusPainted(false);
         musicButton.setOpaque(false);
 
-        musicButton.setBounds(650, 20, 50, 50);
+        musicButton.setBounds((int) (initFrameResize * 650), (int) (initFrameResize * 20), (int) (initFrameResize * 50), (int) (initFrameResize * 50));
         musicButton.setIcon(Button_Light_New);
         musicButton.addMouseListener(new MouseListener() {
             @Override
@@ -168,15 +206,15 @@ public class InitFrame extends JFrame{
 //        System.out.println("Exit button is initializing...");
 
         /** To get the scaled Image */
-        ImageIcon Button_Light_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\AllFrame\\ExitButton_Light.png").getScaledInstance(50, 50, Image.SCALE_SMOOTH));
-        ImageIcon Button_Dark_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\AllFrame\\ExitButton_Dark.png").getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+        ImageIcon Button_Light_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\AllFrame\\ExitButton_Light.png").getScaledInstance((int) (initFrameResize * 50), (int) (initFrameResize * 50), Image.SCALE_SMOOTH));
+        ImageIcon Button_Dark_New = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\AllFrame\\ExitButton_Dark.png").getScaledInstance((int) (initFrameResize * 50), (int) (initFrameResize * 50), Image.SCALE_SMOOTH));
 
         exitButton.setBorderPainted(false);
         exitButton.setContentAreaFilled(false);
         exitButton.setFocusPainted(false);
         exitButton.setOpaque(false);
 
-        exitButton.setBounds(710, 20, 50, 50);
+        exitButton.setBounds((int) (initFrameResize * 710), (int) (initFrameResize * 20), (int) (initFrameResize * 50), (int) (initFrameResize * 50));
         exitButton.setIcon(Button_Light_New);
         exitButton.addMouseListener(new MouseListener() {
             @Override
@@ -204,11 +242,11 @@ public class InitFrame extends JFrame{
 //        System.out.println("InitFrame label is initializing...");
 
         /** To get the scaled image. */
-        ImageIcon newIcon = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\InitFrame\\Jungle.png").getScaledInstance(512, 324, Image.SCALE_SMOOTH));
+        ImageIcon newIcon = new ImageIcon(Toolkit.getDefaultToolkit().getImage("image\\InitFrame\\Jungle.png").getScaledInstance((int) (initFrameResize * 512), (int) (initFrameResize * 324), Image.SCALE_SMOOTH));
 
         JLabel label = new JLabel(newIcon);
         label.setPreferredSize(new Dimension(newIcon.getIconWidth(), newIcon.getIconHeight()));
-        label.setBounds(150, 10, newIcon.getIconWidth(), newIcon.getIconHeight());
+        label.setBounds((int) (initFrameResize * 150), (int) (initFrameResize * 10), newIcon.getIconWidth(), newIcon.getIconHeight());
         this.getContentPane().add(label);
     }
 }
